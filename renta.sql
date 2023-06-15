fd9a6fe29f9a8b1e30c7dd3b5b4947cd7de477d7
wandy141 <115728722+wandy141@users.noreply.github.com>
2023-06-15 13:06:09 -0600

    nueva basede datos limpia

--- renta.sql
+++ renta.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `renta` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `renta`;
--- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: renta
 -- ------------------------------------------------------
--- Server version	8.0.27
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,12 +24,12 @@ DROP TABLE IF EXISTS `alquiler`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alquiler` (
   `idalquiler` int NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `usuario` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `idcliente` int NOT NULL,
-  `nombrecliente` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `nombrecliente` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `fecha` datetime DEFAULT NULL,
   `idvehiculo` int DEFAULT NULL,
-  `seguro` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `seguro` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `precio` double DEFAULT NULL,
   `fechaini` datetime DEFAULT NULL,
   `fechafin` datetime DEFAULT NULL,
@@ -43,7 +41,7 @@ CREATE TABLE `alquiler` (
   PRIMARY KEY (`idalquiler`),
   KEY `fk_vehiculo` (`idvehiculo`),
   CONSTRAINT `fk_vehiculo` FOREIGN KEY (`idvehiculo`) REFERENCES `vehiculo` (`idvehiculo`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +50,7 @@ CREATE TABLE `alquiler` (
 
 LOCK TABLES `alquiler` WRITE;
 /*!40000 ALTER TABLE `alquiler` DISABLE KEYS */;
-INSERT INTO `alquiler` VALUES (12,'wandy',2,'Aqfa','2023-05-19 00:00:00',64,'0',88,'2023-05-19 00:00:00','2023-05-22 00:00:00',4,352,1,NULL,1),(13,'wandy',1,'Hola','2023-05-19 00:00:00',47,'0',55,'2023-05-19 00:00:00','2023-05-22 00:00:00',4,220,0,NULL,1),(14,'wandy',1,'Hola','2023-05-19 00:00:00',47,'0',55,'2023-05-23 00:00:00','2023-05-24 00:00:00',2,110,0,NULL,1),(18,'wandy',2,'Aqfa','2023-06-01 00:00:00',48,'0',44,'2023-06-01 00:00:00','2023-06-23 00:00:00',23,1012,1,NULL,1),(19,'wandy',1,'Hola','2023-06-02 13:47:00',65,'si',85,'2023-06-02 13:47:00','2023-06-03 13:47:00',2,190,0,NULL,1),(20,'wandy',3,'Zd','2023-06-02 13:47:00',62,'no',56,'2023-06-02 13:47:00','2023-06-10 13:47:00',9,504,0,NULL,1),(21,'wandy',2,'Aqfa','2023-06-02 13:47:00',63,'si',50,'2023-06-02 13:47:00','2023-06-10 13:47:00',9,470,0,NULL,1),(22,'wandy',1,'Hola','2023-06-02 14:22:00',64,'normal',88,'2023-06-02 14:22:00','2023-06-03 14:22:00',2,201,0,NULL,1),(23,'wandy',1,'Hola','2023-06-14 08:01:00',47,'normal',55,'2023-06-14 08:01:00','2023-06-15 08:01:00',1,55,0,NULL,1),(24,'wandy',1,'Hola','2023-06-14 08:01:00',48,'normal',44,'2023-06-14 08:01:00','2023-06-15 08:01:00',1,44,0,NULL,1);
+INSERT INTO `alquiler` VALUES (12,'wandy',2,'Aqfa','2023-05-19 00:00:00',64,'0',88,'2023-05-19 00:00:00','2023-05-22 00:00:00',4,352,1,NULL,1),(13,'wandy',1,'Hola','2023-05-19 00:00:00',47,'0',55,'2023-05-19 00:00:00','2023-05-22 00:00:00',4,220,1,NULL,1),(14,'wandy',1,'Hola','2023-05-19 00:00:00',47,'0',55,'2023-05-23 00:00:00','2023-05-24 00:00:00',2,110,0,NULL,1),(18,'wandy',2,'Aqfa','2023-06-01 00:00:00',48,'0',44,'2023-06-01 00:00:00','2023-06-23 00:00:00',23,1012,1,1,2),(19,'wandy',1,'Hola','2023-06-02 13:47:00',65,'si',85,'2023-06-02 13:47:00','2023-06-03 13:47:00',2,190,0,NULL,1),(20,'wandy',3,'Zd','2023-06-02 13:47:00',62,'no',56,'2023-06-02 13:47:00','2023-06-10 13:47:00',9,504,0,NULL,1),(21,'wandy',2,'Aqfa','2023-06-02 13:47:00',63,'si',50,'2023-06-02 13:47:00','2023-06-10 13:47:00',9,470,0,NULL,1),(22,'wandy',1,'Hola','2023-06-02 14:22:00',64,'normal',88,'2023-06-02 14:22:00','2023-06-03 14:22:00',2,201,0,NULL,1),(23,'wandy',1,'Hola','2023-06-14 08:01:00',47,'normal',55,'2023-06-14 08:01:00','2023-06-15 08:01:00',1,55,0,NULL,1),(24,'wandy',1,'Hola','2023-06-14 08:01:00',48,'normal',44,'2023-06-14 08:01:00','2023-06-15 08:01:00',1,44,0,NULL,1),(25,'wandy',1,'Hola','2023-06-15 08:33:00',62,'normal',56,'2023-06-15 08:33:00','2023-06-16 08:33:00',1,56,0,NULL,1);
 /*!40000 ALTER TABLE `alquiler` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,9 +63,11 @@ DROP TABLE IF EXISTS `cliente`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cliente` (
   `idcliente` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `correo` varchar(60) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `cedula` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `correo` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cedula` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `direccion` varchar(455) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `telefono` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`idcliente`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -78,7 +78,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'Hola','eoo','123321'),(2,'Aqfa','asdfas','sdsad'),(3,'Zd','sfcs','szcffs');
+INSERT INTO `cliente` VALUES (1,'Hola','eoo','123321',NULL,NULL),(2,'Aqfa','asdfas','sdsad',NULL,NULL),(3,'Zd','sfcs','szcffs',NULL,NULL);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,13 +93,13 @@ CREATE TABLE `entrega` (
   `identrega` int NOT NULL AUTO_INCREMENT,
   `idalquiler` int NOT NULL,
   `fechahora` datetime NOT NULL,
-  `persona_recibe` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `cedula_persona` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `persona_recibe` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cedula_persona` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `kilometraje` int NOT NULL,
-  `nota` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `nota` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `estado` int DEFAULT '1',
   PRIMARY KEY (`identrega`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -108,8 +108,32 @@ CREATE TABLE `entrega` (
 
 LOCK TABLES `entrega` WRITE;
 /*!40000 ALTER TABLE `entrega` DISABLE KEYS */;
-INSERT INTO `entrega` VALUES (7,18,'2023-06-16 17:24:00','wandy','234',23,'jart',1);
+INSERT INTO `entrega` VALUES (7,18,'2023-06-16 17:24:00','wandy','234',23,'jart',1),(8,13,'2023-06-16 09:01:00','hola papa','77688',876,'el carro tiene todo completo',1);
 /*!40000 ALTER TABLE `entrega` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entregas`
+--
+
+DROP TABLE IF EXISTS `entregas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entregas` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entregas`
+--
+
+LOCK TABLES `entregas` WRITE;
+/*!40000 ALTER TABLE `entregas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entregas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -151,11 +175,11 @@ CREATE TABLE `recibir` (
   `idrecibir` int NOT NULL AUTO_INCREMENT,
   `id_cliente` int DEFAULT NULL,
   `id_alquiler` int DEFAULT NULL,
-  `NombreCli` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `NombreCli` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `FechHoraDev` datetime DEFAULT NULL,
-  `Comentarios` text COLLATE utf8mb4_unicode_ci,
+  `Comentarios` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`idrecibir`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,7 +188,7 @@ CREATE TABLE `recibir` (
 
 LOCK TABLES `recibir` WRITE;
 /*!40000 ALTER TABLE `recibir` DISABLE KEYS */;
-INSERT INTO `recibir` VALUES (1,18,7,'wandy','2023-06-14 08:28:00','waaw'),(2,18,7,'wandy','2023-06-15 18:38:00','se le daño la puerta');
+INSERT INTO `recibir` VALUES (1,18,7,'wandy','2023-06-14 08:28:00','waaw'),(2,18,7,'wandy','2023-06-15 18:38:00','se le daño la puerta'),(3,1,18,'lala','2023-06-16 10:38:00','bienn');
 /*!40000 ALTER TABLE `recibir` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,8 +201,8 @@ DROP TABLE IF EXISTS `rentas`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rentas` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `contraseña` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contraseña` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -202,8 +226,8 @@ DROP TABLE IF EXISTS `seccion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `seccion` (
-  `token` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `usuarioid` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `usuarioid` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `fechavalida` datetime DEFAULT NULL,
   PRIMARY KEY (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -220,6 +244,30 @@ INSERT INTO `seccion` VALUES ('brian2123','brian2','2023-04-14 16:44:47'),('wand
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tipo_vehiculos`
+--
+
+DROP TABLE IF EXISTS `tipo_vehiculos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipo_vehiculos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_vehiculos`
+--
+
+LOCK TABLES `tipo_vehiculos` WRITE;
+/*!40000 ALTER TABLE `tipo_vehiculos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo_vehiculos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tipovehiculo`
 --
 
@@ -228,7 +276,7 @@ DROP TABLE IF EXISTS `tipovehiculo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tipovehiculo` (
   `idtipo` int NOT NULL AUTO_INCREMENT,
-  `tipo` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tipo` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `estado` int DEFAULT NULL,
   PRIMARY KEY (`idtipo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -245,36 +293,6 @@ INSERT INTO `tipovehiculo` VALUES (1,'Economico',1),(2,'Compacto',1),(3,'Deporti
 UNLOCK TABLES;
 
 --
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `usuario`
 --
 
@@ -282,9 +300,9 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `usuarioid` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  `contrasena` varchar(12) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `nombre` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `usuarioid` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `contrasena` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `estado` int DEFAULT NULL,
   PRIMARY KEY (`usuarioid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -309,16 +327,16 @@ DROP TABLE IF EXISTS `vehiculo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vehiculo` (
   `idvehiculo` int NOT NULL AUTO_INCREMENT,
-  `marca` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `modelo` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `color` varchar(25) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `marca` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `modelo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `color` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `asiento` int DEFAULT NULL,
-  `combustible` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `combustible` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `tipo` int DEFAULT NULL,
   `ano` int DEFAULT NULL,
-  `placa` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `placa` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `precio` double DEFAULT NULL,
-  `imagen` varchar(555) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `imagen` varchar(555) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `mantenimiento` int DEFAULT '0',
   `estado` int DEFAULT '1',
   PRIMARY KEY (`idvehiculo`),
@@ -333,13 +351,9 @@ CREATE TABLE `vehiculo` (
 
 LOCK TABLES `vehiculo` WRITE;
 /*!40000 ALTER TABLE `vehiculo` DISABLE KEYS */;
-INSERT INTO `vehiculo` VALUES (47,'Toyota','Banglades','Gris',5,'Gasolina',1,2022,'H28888',55,'211529-carro-deportivo.png',0,1),(48,'Honda','logo','blanco',4,'Gasolina',1,2022,'A656530',44,'212111-carro-deportivo.png',0,1),(61,'Tesla','xr33','blanco',4,'Electrico',5,2022,'xvxffxggfcff',76,'212220-carro-deportivo.png',0,1),(62,'toyota','asdf78','rojo',5,'Gas',2,2006,'a382',56,'130103-carros-mas-caros-Lamborghini-Aventador.jpg',0,1),(63,'Toyota','Camry','Rojo',4,'Gasolina',2,2018,'VHGVhsw2',50,NULL,0,1),(64,'Casd','Fsdf','Aerg',4,'Gasolina',6,2010,'Ergre',88,NULL,0,1),(65,'Th','Fghf','Tfdth',4,'Gasoil',3,2008,'Fhfd',85,NULL,0,1),(66,'Fdg','Df','G',2,'Gasolina',4,2005,'Fg',85,NULL,0,1),(67,'Ww','Ww','Ww',2,'Gasolina',3,2008,'Ww',7,NULL,0,1),(68,'Dfv','Sdv','Dasfv',2,'Gasoil',3,2008,'Dsfv',78.9,NULL,0,0),(77,'Toyota','XX','Rojo',4,'Gasolina',3,2021,'A142415',14250,'212352-logo14.jpg',0,1);
+INSERT INTO `vehiculo` VALUES (47,'Toyota','Banglades','Gris',5,'Gasolina',1,2022,'H28888',55,'211529-carro-deportivo.png',0,1),(48,'Honda','logo','blanco',4,'Gasolina',1,2022,'A656530',44,'212111-carro-deportivo.png',0,2),(61,'Tesla','xr33','blanco',4,'Electrico',5,2022,'xvxffxggfcff',76,'212220-carro-deportivo.png',0,1),(62,'toyota','asdf78','rojo',5,'Gas',2,2006,'a382',56,'182020-sonata_2022.jpg',0,1),(63,'Toyota','Camry','Rojo',4,'Gasolina',2,2018,'VHGVhsw2',50,'crv.jpg',0,1),(64,'Casd','Fsdf','Aerg',4,'Gasolina',6,2010,'Ergre',88,'crv.jpg',0,1),(65,'Th','Fghf','Tfdth',4,'Gasoil',3,2008,'Fhfd',85,'crv.jpg',0,1),(66,'Fdg','Df','G',2,'Gasolina',4,2005,'Fg',85,'crv2.jpg',0,1),(67,'Ww','Ww','Ww',2,'Gasolina',3,2008,'Ww',7,'crv2.jpg',0,1),(68,'Dfv','Sdv','Dasfv',2,'Gasoil',3,2008,'Dsfv',78.9,'crv.jpg',0,0),(77,'Toyota','XX','Rojo',4,'Gasolina',3,2021,'A142415',14250,'173221-sonata_2022.jpg',0,1);
 /*!40000 ALTER TABLE `vehiculo` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'renta'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -350,4 +364,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-14 22:35:38
+-- Dump completed on 2023-06-15 12:48:32
